1664d104a90d4e35ad1446ef6f50db44882be1c4
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-18 11:19:58 +0400

    решение task0

--- task0/Program.cs
+++ task0/Program.cs
@@ -3,3 +3,22 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.WriteLine("Введите число A: ");
+long a = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+long b = Convert.ToInt64(Console.ReadLine());
+
+long GetPower(long a1, long b1)
+{
+    long power = 1;
+    for (long i = 1; i <= Math.Abs(b1); i++)
+    {
+        power = power * a1;
+    }
+    return power;
+}
+
+long result = GetPower(a, b);
+Console.WriteLine($"Число {a} в степени |{b}| = {result}");
